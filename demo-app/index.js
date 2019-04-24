df62ef2390b37f5e49e89b0728b16fc1662b5755
sven4ask <sven@legalthings.io>
2019-04-24 13:51:46 +0200

    Add novum workflow a possible scenario to go through the steps

--- demo-app/index.js
+++ demo-app/index.js
@@ -7,28 +7,89 @@ const main = async () => {
   const lto = new LTOHelper(nodeUrl);
 
   // Create KeyPairs for both participants
-  const initiator = lto.createAccount('the seed of nda initiator');
+  const orphan = lto.createAccount('the seed of orphan');
+  const onderwijsinstelling = lto.createAccount('the seed of onderwijsinstelling');
+  const gba = lto.createAccount('the seed of gba');
 
   // Initialize an empty chain
-  let chain = lto.createEventChain(initiator, 'process-identifier');
+  let chain = lto.createEventChain(orphan, 'process-identifier');
 
   //Based on the chain we can generate resource ids for resources like actors and the process
   const processId = chain.createProjectionId('main');
-  initiator.id = chain.createProjectionId('initiator');
+  orphan.id = chain.createProjectionId('orphan');
+  onderwijsinstelling.id = chain.createProjectionId('onderwijsinstelling');
+  gba.id = chain.createProjectionId('gba');
 
   // For testing purposes remove the chain if it already exists
-  await lto.deleteEventChain(initiator, chain.id);
+  await lto.deleteEventChain(orphan, chain.id);
+  await lto.deleteProcess(orphan, processId);
 
   // To be able to interact with the chain the identity (with it's public key) needs to be added to the chain
-  chain = await lto.addIdentityEvent(chain, initiator, 'initiator');
+  chain = await lto.addIdentityEvent(chain, orphan, 'orphan');
+
+  // To be able to interact with the chain the identity (with it's public key) needs to be added to the chain
+  // Only the initiator is allowed to add new identities, this event needs to be signed by the initiator
+  chain = await lto.addIdentityEvent(chain, onderwijsinstelling, 'onderwijsinstelling', orphan);
+
+  // To be able to interact with the chain the identity (with it's public key) needs to be added to the chain
+  // Only the initiator is allowed to add new identities, this event needs to be signed by the initiator
+  chain = await lto.addIdentityEvent(chain, gba, 'gba', orphan);
 
   // Load the scenario and add it to the chain
   const scenario = require(`./scenarios/empty.json`);
   scenario.id = sha256(JSON.stringify(scenario)).toString();
-  chain = lto.addScenarioEvent(chain, initiator, scenario);
+  chain = lto.addScenarioEvent(chain, orphan, scenario);
+
+  // Start the process
+  const actors = {
+    orphan: orphan.id,
+    onderwijsinstelling: onderwijsinstelling.id,
+    gba: gba.id
+  };
+  chain = lto.addProcessInitationEvent(chain, orphan, scenario.id, processId, actors);
+
+  // Send the initial data from the orphan
+  const data = {
+    older_18: true,
+    older_21: false,
+    studying: false
+  };
+  chain = lto.addResponseEvent(chain, orphan, processId, 'start', 'ok', data);
 
   try {
-    const res = await lto.sendChain(initiator, chain);
+    const res = await lto.sendChain(orphan, chain);
+    console.log('Started');
+    //console.log(res.data);
+  } catch (e) {
+    console.log(e.response.data);
+    return;
+  }
+
+  // The educational instatution will set the that the orphan is styding
+  const studyData = {
+    studying: true
+  };
+  chain = lto.addResponseEvent(chain, onderwijsinstelling, processId, 'receive_info_oi', 'ok', studyData);
+
+  try {
+    const res = await lto.sendChain(onderwijsinstelling, chain);
+    console.log('Started studying');
+    // console.log(res.data);
+  } catch (e) {
+    console.log(e.response.data);
+    return;
+  }
+
+  chain = await lto.loadChain(onderwijsinstelling, chain.id);
+
+  // The gba will change the age of the orphan to older then 21
+  const ageData = {
+    older_21: true
+  };
+  chain = lto.addResponseEvent(chain, gba, processId, 'receive_info_gba', 'ok', ageData);
+
+  try {
+    const res = await lto.sendChain(gba, chain);
     console.log(res.data);
   } catch (e) {
     console.log(e.response.data);
